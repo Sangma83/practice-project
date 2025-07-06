4cf62567e45f07c280fbf8eda1a19c779ade8206
Sangma83 <poushalisangma13@gmail.com>
2025-07-06 18:31:18 +0600

    basic structure of api add

--- project.js
+++ project.js
@@ -1,7 +1,28 @@
 const loadAllProduct = () => {
     fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    displayProduct(data);
+});
 }
 
-loadAllProduct()
+
+const displayProduct = (products) => {
+    const productContains = document.getElementById("product-container");
+
+    products.forEach(product => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+        <img src="" alt="" />
+        <h5>h</h5>
+        <p></p>
+        <button>Details</button>
+        <button>Add To Cart</button>
+        `;
+
+        productContains.appendChild(div);
+    });
+};
+
+loadAllProduct();
