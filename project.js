dba4902255d245ebe28d2d541275bd772c298c32
Sangma83 <poushalisangma13@gmail.com>
2025-07-08 00:03:31 +0600

    add cart functionality

--- project.js
+++ project.js
@@ -20,11 +20,26 @@ const displayProduct = (products) => {
         <h3>price: ${product.price} $</h3>
         <p>${product.description.slice(0, 50)}</p>
         <button>Details</button>
-        <button>Add To Cart</button>
+        <button onclick="handleAddToCart('${product.title.slice(0,10)}','${product.price}')">Add To Cart</button>
         `;
 
         productContains.appendChild(div);
     });
+};
+
+
+const handleAddToCart = (name, price) => {
+    //console.log(name, price);
+    const container = document.getElementById("cart-main-container");
+
+    const div = document.createElement("div");
+    div.classList.add("cart-info");
+    div.innerHTML = `
+    <p>${name}</p>
+    <h3>${price}</h3>
+
+    `;
+    container.appendChild(div);
 };
 
 loadAllProduct();
