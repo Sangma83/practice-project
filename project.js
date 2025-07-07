a6355d5c598e47b86ee3c259c8ee425495134149
Sangma83 <poushalisangma13@gmail.com>
2025-07-07 23:37:47 +0600

    add details interface of api

--- project.js
+++ project.js
@@ -11,12 +11,14 @@ const displayProduct = (products) => {
     const productContains = document.getElementById("product-container");
 
     products.forEach(product => {
+        console.log(product);
         const div = document.createElement("div");
-
+        div.classList.add("card");
         div.innerHTML = `
-        <img src="" alt="" />
-        <h5>h</h5>
-        <p></p>
+        <img class="card-img" src=${product.image} alt="" />
+        <h5>${product.title}</h5>
+        <h3>price: ${product.price} $</h3>
+        <p>${product.description.slice(0, 50)}</p>
         <button>Details</button>
         <button>Add To Cart</button>
         `;
